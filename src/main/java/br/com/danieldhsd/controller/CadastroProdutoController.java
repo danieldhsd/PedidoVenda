b5028ca5b7bf008cdde44a68b6ed5150ce827f5b
Daniel Dutra <danieldhsd@gmail.com>
2020-06-18 12:57:51 -0300

    Tratando excessao view expired, outras execessoes e logging de mensagens e erros com log4j

---
+++ src/main/java/br/com/danieldhsd/controller/CadastroProdutoController.java
@@ -0,0 +1,10 @@
+package br.com.danieldhsd.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class CadastroProdutoController {
+
+}
