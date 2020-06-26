b3b5be501050d6fbd90a160f53606b1c16ca6187
Daniel Dutra <danieldhsd@gmail.com>
2020-06-26 12:43:32 -0300

    Pequenas alteracoes

--- src/main/java/br/com/danieldhsd/controller/CadastroProdutoController.java
+++ src/main/java/br/com/danieldhsd/controller/CadastroProdutoController.java
@@ -1,11 +1,15 @@
 package br.com.danieldhsd.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
+import br.com.danieldhsd.model.Categoria;
 import br.com.danieldhsd.model.Produto;
 
 @Named
@@ -14,14 +18,30 @@ public class CadastroProdutoController implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	private EntityManager manager;
+	
+	private List<Categoria> categoriasRaizes;
+	
 	private Produto produto;
 	
 	@PostConstruct
 	public void init() {
 		produto = new Produto();
+		inicializar();
 	}
 
+	public void inicializar() {
+		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class)
+							.getResultList();
+	}
+	
 	public Produto getProduto() {
 		return produto;
 	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
 }
--- src/main/java/br/com/danieldhsd/model/Grupo.java
+++ src/main/java/br/com/danieldhsd/model/Grupo.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Grupo")
+@Table(name = "GRUPO")
 public class Grupo {
 	
 	@Id
